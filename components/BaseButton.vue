96e1c553de6074112b1eb3f04cd1c687a96df74c
Patrik Slovic <patrikslovic@gmail.com>
2021-07-15 03:23:51 +0200

    init :tada:

---
+++ components/BaseButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <nuxt-link v-if="link" class="container" :to="to">
+    <button class="btn">{{ buttonText }}</button>
+  </nuxt-link>
+
+  <button v-else class="btn">{{ buttonText }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonText: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    link: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    to: {
+      type: Object,
+      required: false,
+      default: () => ({}),
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  background: blue;
+}
+
+.btn {
+  border: 2px #283558 solid;
+  border-radius: 8px;
+  padding: 10px 10px;
+  width: 400px;
+  font-size: 26px;
+  text-transform: uppercase;
+  margin: 10px;
+  text-align: center;
+  background: #3c5186;
+  color: #fff5de;
+  margin: 10px 0;
+}
+
+.btn:hover {
+  color: #3c5186;
+  background: #fff5de;
+}
+
+.btn:focus {
+  outline: 0;
+  box-shadow: none;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <body>
+    <main>
+      <div class="buttons">
+        <nuxt-link to="/gameSettings"><BaseButton class="btn" :buttonText="'New game'"/></nuxt-link>
+        <BaseButton class="btn" :buttonText="'Options'" />
+        <BaseButton class="btn" :buttonText="'Exit'" />
+      </div>
+    </main>
+  </body>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+main {
+  margin: 0 auto;
+  width: 80%;
+}
+
+.buttons {
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+}
+</style>
