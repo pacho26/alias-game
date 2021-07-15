1adb7c14274684f2da13ecb9c8af03f9ea9687da
Patrik Slovic <patrikslovic@gmail.com>
2021-07-15 03:33:59 +0200

    added default layout (header)

---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+      rel="stylesheet"
+    />
+    <header>
+      <nuxt-link to="/" id="logoLink">Alias<span>Game</span>
+      </nuxt-link>
+    </header>
+    <Nuxt />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+* {
+  font-family: 'Poppins', sans-serif;
+}
+
+#logoLink {
+  text-decoration: none;
+  font-family: 'Poppins Bold', sans-serif;
+  color: #3c5186;
+  font-size: 40px;
+  font-weight: 800;
+  padding: 8px;
+  user-select: none;
+
+  span {
+    font-family: 'Poppins Extra Bold', sans-serif;
+    color: #9b72aa;
+    font-size: 20px;
+    font-weight: 800;
+  }
+}
+
+header {
+  margin: 0;
+  padding: 0;
+  background: #fff5de;
+}
+</style>
