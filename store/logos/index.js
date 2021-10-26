38a3616e32bd347afd1e4de84b2591078553807b
Patrik Slovic <patrikslovic@gmail.com>
2021-10-26 16:54:49 +0200

    refactor logo selection:hammer:

---
+++ store/logos/index.js
@@ -0,0 +1,440 @@
+export const state = () => ({
+  logos: [
+    {
+      category: 'football',
+      images: [
+        {
+          token: 'bbcf8996-810e-45a4-9428-af1947021020',
+          alt: 'barcelona',
+        },
+        {
+          token: '1b29eeeb-5bf2-4b78-8321-df0819645517',
+          alt: 'bayern',
+        },
+        {
+          token: '8460c2af-4f0a-499f-b9a0-f80f758a7efc',
+          alt: 'borussia',
+        },
+        {
+          token: '44556fdc-0ccc-40f0-ad52-0708298c6e7f',
+          alt: 'chelsea',
+        },
+        {
+          token: 'adfa968c-ca5d-453f-bb3c-e2733512ba59',
+          alt: 'inter',
+        },
+        {
+          token: 'd29babb2-ac88-423d-990b-fc6a8be39fe8',
+          alt: 'juventus',
+        },
+        {
+          token: '9aaae7d9-4f82-4fcf-8744-7a6fffc78dc7',
+          alt: 'manchester-city',
+        },
+        {
+          token: '3f16dc0b-b429-41d1-b268-f808e3d6f73f',
+          alt: 'milan',
+        },
+        {
+          token: 'a648e4a7-2f9b-468b-9da2-add208921ef5',
+          alt: 'psg',
+        },
+        {
+          token: '6cba7e73-c1c7-4dea-8be3-8d04d3a25f07',
+          alt: 'real-madrid',
+        },
+        {
+          token: 'f62e0b41-8c15-4579-88ea-a109d5a3c65e',
+          alt: 'manchester-united',
+        },
+      ],
+    },
+    {
+      category: 'companies',
+      images: [
+        {
+          token: '766884e3-c1c6-4ea9-b9ec-4ca393ff717e',
+          alt: 'aliexpress',
+        },
+        {
+          token: '33199418-7902-4926-944a-c84ffc524ce3',
+          alt: 'amazon',
+        },
+        {
+          token: '87879a6b-94b1-478f-8d15-c8b6fecc5bc9',
+          alt: 'ebay',
+        },
+        {
+          token: '76f67270-58c4-45a9-bc04-b6667d91e3d1',
+          alt: 'facebook',
+        },
+        {
+          token: 'c34621ca-0295-4420-9bf0-04410ad050ae',
+          alt: 'google',
+        },
+        {
+          token: '872dc56b-c9a7-46d3-888f-888116ba4afe',
+          alt: 'instagram',
+        },
+        {
+          token: '589ba059-66ee-4598-b6a2-ddd9234047af',
+          alt: 'mcdonalds',
+        },
+        {
+          token: '223d35af-7e21-41a6-959e-afd9ed0b29a8',
+          alt: 'netflix',
+        },
+        {
+          token: 'aa68b13f-98d0-49a5-b85d-1fc9b6ed0ccd',
+          alt: 'tiktok',
+        },
+        {
+          token: '60c8181c-202a-4313-ad08-55e6d1a98d00',
+          alt: 'youtube',
+        },
+      ],
+    },
+    {
+      category: 'fancy',
+      images: [
+        {
+          token: 'ffda2bb1-0663-4dca-83b2-6bb44b009512',
+          alt: 'pikachu',
+        },
+        {
+          token: 'd9a97b44-7d61-4c92-a42b-ad6651ada140',
+          alt: 'meowth',
+        },
+        {
+          token: '58fbd09b-f697-4817-a3a4-f077799640a2',
+          alt: 'psyduck',
+        },
+        {
+          token: '9a3203df-fd58-48d9-8c01-47cd4610e125',
+          alt: 'goku',
+        },
+        {
+          token: '08167f69-8e90-4a27-80f6-dcdb51b64ee0',
+          alt: 'super-mario',
+        },
+        {
+          token: '84037327-5103-4074-809c-9ea919117961',
+          alt: 'turtle1',
+        },
+        {
+          token: '8b5d6bf9-e611-4967-9dba-e660ca7a48e6',
+          alt: 'turtle2',
+        },
+        {
+          token: '1d7d7fe7-282e-4d0f-bc1e-c45b2a129736',
+          alt: 'turtle3',
+        },
+        {
+          token: '9494eb5b-9405-402e-b750-a7c841802ba0',
+          alt: 'turtle4',
+        },
+        {
+          token: 'e8c3546a-d04f-48cc-a6db-2b23f425ae89',
+          alt: 'homer',
+        },
+        {
+          token: 'a23d8340-5af0-4cda-805c-ec67095b0b18',
+          alt: 'bart',
+        },
+        {
+          token: 'f6529aac-9cbd-42d7-a635-5a3ce6cc99d9',
+          alt: 'johnny-bravo',
+        },
+        {
+          token: '200736ed-a91e-4a3b-914e-306bfda2a2f3',
+          alt: 'spongebob',
+        },
+        {
+          token: '90e32e27-54c1-4c1f-b9e5-1ae45f606662',
+          alt: 'patrick',
+        },
+        {
+          token: '04a6d6f5-8115-457a-a2d0-fece1fa5d909',
+          alt: 'pink-panther',
+        },
+        {
+          token: 'a4b51885-f18e-422d-bfdc-9b368666e4b8',
+          alt: 'bugs-bunny',
+        },
+      ],
+    },
+    {
+      category: 'cars',
+      images: [
+        {
+          token: '4279d4cb-5425-4ae9-b036-62da14dcdc92',
+          alt: 'alfa-romeo',
+        },
+        {
+          token: '1a810ba7-0e72-4740-9bf7-d9557cb46214',
+          alt: 'citroen',
+        },
+        {
+          token: '9b093ab1-7f63-4c9a-959c-804e7be3cdaf',
+          alt: 'ferrari',
+        },
+        {
+          token: '0f5fc126-effe-4987-a2d3-5121a39df332',
+          alt: 'ford',
+        },
+        {
+          token: 'f43fca72-a0db-41b1-895e-4a1cc9ed25ca',
+          alt: 'hyundai',
+        },
+        {
+          token: '141a72be-e57a-42d8-a24d-1aa80507f07e',
+          alt: 'lamborghini',
+        },
+        {
+          token: '9643eb7b-d64e-4707-98eb-105723eda612',
+          alt: 'opel',
+        },
+        {
+          token: '994cadb3-5b38-4dcf-abd5-16f83d527824',
+          alt: 'renault',
+        },
+        {
+          token: '368954ed-5a5b-4c1e-8041-6c6a0fcd7491',
+          alt: 'rimac',
+        },
+        {
+          token: '61882f7a-e629-47f5-b08c-01381dc88eb0',
+          alt: 'skoda',
+        },
+        {
+          token: '04429a59-1610-4ec8-a848-fb599f206b3a',
+          alt: 'tesla',
+        },
+        {
+          token: '0d968e86-77d3-4c85-a976-9ce24069f3d1',
+          alt: 'volkswagen',
+        },
+      ],
+    },
+    {
+      category: 'food',
+      images: [
+        {
+          token: '74e8dbaf-358b-4b45-ad3b-3ef386002edf',
+          alt: 'apple',
+        },
+        {
+          token: 'd54518b0-58d0-4fa7-98b1-b21b6f2f9107',
+          alt: 'avocado',
+        },
+        {
+          token: 'f95b8b43-860c-4331-98d1-0e09d61af9f2',
+          alt: 'burger',
+        },
+        {
+          token: '2f413499-5bc4-42ba-a38e-db814463ba60',
+          alt: 'cupcake',
+        },
+        {
+          token: '240d4c8e-0749-44f7-8a51-42a0430b27b9',
+          alt: 'french-fries',
+        },
+        {
+          token: '9360cc4f-d8a2-4f35-a0fa-ef370efb6c56',
+          alt: 'gingerbread-man',
+        },
+        {
+          token: 'c8bc055a-2d41-4019-87cc-989012666549',
+          alt: 'hotdog',
+        },
+        {
+          token: '252ed22a-0920-4c48-86a0-0446a766707c',
+          alt: 'macarons',
+        },
+        {
+          token: '8a99a3aa-d8fc-4407-950e-af2e16d1659d',
+          alt: 'mushroom',
+        },
+        {
+          token: '5e0cf63d-b6b2-4aae-8a4e-5dac4cded01e',
+          alt: 'pineapple',
+        },
+        {
+          token: 'cda26986-8f30-4883-91a5-fc21e59365af',
+          alt: 'pizza',
+        },
+        {
+          token: '2af914c7-8c36-4fee-8290-21a69db21470',
+          alt: 'strawberry',
+        },
+        {
+          token: 'acc8e6e5-19d9-44bf-a1d8-762fbcb1a8d5',
+          alt: 'watermelon',
+        },
+      ],
+    },
+    {
+      category: 'it',
+      images: [
+        {
+          token: '36d6ef1b-3b56-4421-ad2c-5b0b3f6fa63a',
+          alt: 'apple',
+        },
+        {
+          token: 'fb538aab-f27b-4a2e-a758-48ce7a103287',
+          alt: 'linux',
+        },
+        {
+          token: '9547228d-0192-40e5-9ff2-bc7550301560',
+          alt: 'windows',
+        },
+        {
+          token: '56b98231-3430-41f0-8322-65df79759074',
+          alt: 'android',
+        },
+        {
+          token: '11ac2390-cb78-4b55-85d7-75a3e02ef767',
+          alt: 'html',
+        },
+        {
+          token: 'aea6d7be-7899-4deb-9cd2-fa76a83d9a31',
+          alt: 'css',
+        },
+        {
+          token: '991433bd-efbb-4753-840c-c98aa052bc3c',
+          alt: 'javascript',
+        },
+        {
+          token: 'eede36ed-7508-4303-942b-aaf9ca6fff02',
+          alt: 'typescript',
+        },
+        {
+          token: 'ba59a0cd-8ec7-4f5d-b7d4-df85c4d92fbe',
+          alt: 'angular',
+        },
+        {
+          token: '609ccfcd-3626-460d-80cc-4342774f5e08',
+          alt: 'react',
+        },
+        {
+          token: '20b7f478-ad59-4c84-a3f6-bb4b2ca9cd6e',
+          alt: 'vue',
+        },
+        {
+          token: '06e7a2b3-4ac2-4283-b11b-07f423f3b428',
+          alt: 'nuxt',
+        },
+        {
+          token: 'e7eae2e9-d116-4e88-994b-a305ed4d625b',
+          alt: 'java',
+        },
+        {
+          token: '4fbff1c5-7032-492b-a409-3086cf3d6b84',
+          alt: 'cpp',
+        },
+        {
+          token: 'b2e3e573-1008-4a3d-872d-1b06d1cf24b7',
+          alt: 'cs',
+        },
+        {
+          token: '9a897678-92ec-42ad-83d4-bdb2cc19d0b6',
+          alt: 'python',
+        },
+        {
+          token: 'f7c0f2b7-ce60-4d70-a688-0ec8b5739e86',
+          alt: 'go',
+        },
+        {
+          token: 'd958d461-f105-4b3d-ad62-20de6cf93868',
+          alt: 'mysql',
+        },
+        {
+          token: 'c2d935f2-a618-4315-8b28-b5ef13a7cf87',
+          alt: 'postgre',
+        },
+        {
+          token: '4b4d6950-4913-4006-b9d8-055f0e6599a3',
+          alt: 'firebase',
+        },
+        {
+          token: '74d6296f-2caa-4296-b9dd-04ebe3802b5d',
+          alt: 'unity',
+        },
+        {
+          token: '4a93cccb-8f86-4c67-99fc-2ec7239dcb3c',
+          alt: 'unreal',
+        },
+      ],
+    },
+    {
+      category: 'alcohol',
+      images: [
+        {
+          token: '100bd9e9-7a6b-4f78-94a4-187b240082ce',
+          alt: 'aba',
+        },
+        {
+          token: '652a4e91-17e4-4b41-8aeb-afd767b1655d',
+          alt: 'hirter',
+        },
+        {
+          token: '58a9a752-8462-49fe-b0b3-b03fdd56457d',
+          alt: 'karlovacko',
+        },
+        {
+          token: '253708f6-d5d1-4437-9e1f-322fec0281c5',
+          alt: 'ozujsko',
+        },
+        {
+          token: 'fc60349c-65ff-486a-9abd-522986cf779a',
+          alt: 'kozel',
+        },
+        {
+          token: 'e4c5ee7a-6a87-48e8-9525-d6ad7066b6f2',
+          alt: 'stephans-brau',
+        },
+        {
+          token: '710399dd-ff3b-42ed-98b1-b8d368763ccb',
+          alt: 'stari-lisac',
+        },
+        {
+          token: 'acd54b81-cd2e-4cd4-ab66-9b790c41f16a',
+          alt: 'blacky',
+        },
+        {
+          token: '049295b7-0bea-4728-af0a-6d9ece474109',
+          alt: 'chivas',
+        },
+        {
+          token: '726dd63f-f2ae-472c-8274-9f57de830475',
+          alt: 'jagermeister',
+        },
+        {
+          token: 'e575cce8-bb8c-4e20-a534-5df0c57eb7d1',
+          alt: 'pelinkovac',
+        },
+        {
+          token: 'b0a9e9cc-0f0c-4e3d-9b28-138fbdba0cf2',
+          alt: 'stock',
+        },
+        {
+          token: '58b7b38b-4e71-4e9d-8d2f-a3dabdf7b916',
+          alt: 'gemist',
+        },
+      ],
+    },
+  ],
+});
+
+export const getters = {
+  getLogos(state) {
+    return state.logos;
+  },
+  getNumberOfLogos(state) {
+    let counter = 0;
+    state.logos.forEach((category) => {
+      counter += category.images.length;
+    });
+    return counter;
+  },
+};
